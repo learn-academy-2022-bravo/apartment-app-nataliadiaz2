bf0852e93c06427fc35f4d4a4c552c3d9b858164
nataliadiaz2 <natdiaz2001@gmail.com>
2022-05-17 15:24:25 -0700

    Home-Page Done

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react'
+import { Nav, NavItem } from 'reactstrap'
 
 export default class Footer extends Component {
   render() {
+    const {
+      logged_in,
+      current_user,
+      new_user_route,
+      sign_in_route,
+      sign_out_route
+    } = this.props
+    console.log("logged_in:", logged_in)
+    console.log("current_user:", current_user)
     return (
-      <h3>Footer</h3>
+      <>
+        {/* <h3>Footer</h3> */}
+        <p>&copy; Natalia Diaz </p>
+        <Nav>
+          {logged_in &&
+            <NavItem>
+              <a href={sign_out_route} className="nav-link">Sign Out</a>
+            </NavItem>
+          }
+          {!logged_in &&
+            <NavItem>
+              <a href={sign_in_route} className="nav-link">Sign In</a>
+            </NavItem>
+          }
+          {!logged_in &&
+            <NavItem>
+              <a href={new_user_route} className="nav-link">Sign Up</a>
+            </NavItem>
+          }
+        </Nav>
+      </>
     )
   }
 }
--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export default class Home extends Component {
   render() {
     return (
-      <h3>Home</h3>
+      <>
+        <h3 className="homeHeader">Apartments Available</h3>
+        <p className="homeText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis enim. Diam ut venenatis tellus in metus vulputate eu. Leo in vitae turpis massa sed elementum tempus egestas sed. Orci a scelerisque purus semper eget. Risus nullam eget felis eget nunc lobortis mattis aliquam. Nunc eget lorem dolor sed viverra ipsum. Sit amet aliquam id diam maecenas ultricies mi. At ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget. Orci a scelerisque purus semper eget duis at. Tortor at auctor urna nunc id cursus metus aliquam. Facilisi nullam vehicula ipsum a arcu cursus vitae congue. Quam viverra orci sagittis eu volutpat odio facilisis. Vitae sapien pellentesque habitant morbi tristique senectus et netus et</p>
+        <img classname="homeImage" src="http://www.contemporist.com/wp-content/uploads/2019/10/modern-apartment-design-hidden-lighting-111019-1249-01-800x408.jpg" />
+      </>
     )
   }
 }
